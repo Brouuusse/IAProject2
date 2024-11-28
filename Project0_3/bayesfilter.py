15aa1c2f9ca4de7a2e491d38926d45f423e4f00c
Thomas Noël <thomas.noel@student.uliege.be>
2024-11-28 17:13:40 +0100

    jeudi_28

--- Project0_3/bayesfilter.py
+++ Project0_3/bayesfilter.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats
 from pacman_module.game import Agent, Directions, manhattanDistance
 
+#+ DICTIONNAIRE (retenir mvt pour ne pas refaire les mêmes "erreurs" (e.g : 4 nuages de points, Pacman au milieu))
 
 class BeliefStateAgent(Agent):
     """Belief state agent.
@@ -44,9 +45,9 @@ class BeliefStateAgent(Agent):
                             if manhattanDistance((k, l), position) < manhattanDistance((i, j), position):
                                 T_t[i][j][k][l] = 1
                             else:
-                                if self.ghostType == "terrified":
+                                if self.ghost == "terrified":
                                     T_t[i][j][k][l] = 8
-                                elif self.ghostType == "afraid":
+                                elif self.ghost == "afraid":
                                     T_t[i][j][k][l] = 2
                                 else:
                                     T_t[i][j][k][l] = 1
@@ -77,8 +78,12 @@ class BeliefStateAgent(Agent):
                     continue
 
                 true_distance = manhattanDistance(position, (x, y))
+                k = abs(evidence - true_distance)
+                if k >= 2:
+                    O_t[x, y] = 0
+                    continue
 
-                noise_probability = scipy.stats.binom.pmf(evidence - true_distance + n, n, p)
+                noise_probability = scipy.stats.binom.pmf(k, n, p)
                 O_t[x, y] = noise_probability
 
         O_t /= np.sum(O_t)
@@ -168,16 +173,6 @@ class PacmanAgent(Agent):
         super().__init__()
         
     def getLegalActions(self, position, walls):
-        """
-        Détermine les mouvements possibles pour Pacman.
-
-        Arguments:
-            position: La position actuelle de Pacman (x, y).
-        walls: Le W x H grid des murs.
-
-        Returns:
-            Une liste de mouvements possibles (Directions.NORTH, Directions.SOUTH, etc.).
-        """
         x, y = position
         actions = []
 
@@ -195,50 +190,144 @@ class PacmanAgent(Agent):
 
         return actions
 
-
     def _get_action(self, walls, beliefs, eaten, position):
         """
-        Arguments:
-            walls: The W x H grid of walls.
-            beliefs: The list of current ghost belief states.
-            eaten: A list of booleans indicating which ghosts have been eaten.
-            position: The current position of Pacman.
+    Arguments:
+        walls: The W x H grid of walls.
+        beliefs: The list of current ghost belief states.
+        eaten: A list of booleans indicating which ghosts have been eaten.
+        position: The current position of Pacman.
 
-        Returns:
-            A legal move as defined in `game.Directions`.
+    Returns:
+        A legal move as defined in `game.Directions`.
         """
-        direction_deltas = {
-            Directions.NORTH: (0, 1),
-            Directions.SOUTH: (0, -1),
-            Directions.EAST: (1, 0),
-            Directions.WEST: (-1, 0),
-            Directions.STOP: (0, 0)
-        }
 
         legal_actions = self.getLegalActions(position, walls)
 
-        best_action = Directions.STOP
-        min_distance = float('inf')
-
+        max_belief_positions = []
+        
+        print(beliefs)
+        
+        
         for i, belief in enumerate(beliefs):
-            if eaten[i]:
-                continue
+            
+            print("belief : ", belief)
+            print("i : ", i)
+            print("eaten : ", eaten)
+            if not eaten[i]:
+                max_belief_value = np.max(belief)
+                max_belief_positions.append([(x, y) for x in range(belief.shape[0]) for y in range(belief.shape[1]) if belief[x, y] == max_belief_value])
 
-            max_belief_position = np.unravel_index(np.argmax(belief), belief.shape)
+        best_position = None
+        if max_belief_positions:
+            all_max_positions = [pos for sublist in max_belief_positions for pos in sublist]
+            best_position = min(all_max_positions, key=lambda pos: manhattanDistance(position, pos))
 
-            for action in legal_actions:
-                dx, dy = direction_deltas[action]
-                new_position = (position[0] + dx, position[1] + dy)
+        best_action = None
+        if best_position:
+            x, y = position
+            best_x, best_y = best_position
+            # Calcul de la direction vers la meilleure position
+            if best_x < x:
+                best_action = Directions.WEST
+            elif best_x > x:
+                best_action = Directions.EAST
+            elif best_y < y:
+                best_action = Directions.SOUTH
+            elif best_y > y:
+                best_action = Directions.NORTH
 
-                distance = manhattanDistance(new_position, max_belief_position)
+        # Vérifier que la best_action fait partie des actions légales
+        if best_action in legal_actions:
+            return best_action
+        else:
+            # Si aucune direction n'est possible parmi les meilleures options, retourner Directions.STOP
+            return Directions.STOP
 
-                if distance < min_distance:
-                    min_distance = distance
-                    best_action = action
+        
 
-        # Retourne la meilleure action
-        return best_action
+        #1) récupération des légales actions
+        #2) définir une best_action (init à Directions.STOP (départ))
+        #3) la meilleure action est celle qui nous rapproche le plus du centre du nuages de points 
 
+        # #Récupération legal action
+        # legal_actions = self.getLegalActions(position, walls)
+        
+        # #Position goal
+        # pos_goal = max(beliefs)
+        # pos_pacman = position
+        # ref_dist = manhattanDistance(pos_goal, pos_pacman)
+        # print(ref_dist)
+        # best_action = Directions.STOP
+
+        # print(legal_actions)
+        
+        # for actions in legal_actions:
+        #     if actions == Directions.SOUTH:
+        #         tmp_dist = manhattanDistance(pos_goal, (pos_pacman[0], pos_pacman[1] - 1))[0]
+        #         print(tmp_dist)
+        
+        #     elif actions == Directions.EAST:
+        #         tmp_dist = manhattanDistance(pos_goal, (pos_pacman[0] - 1, pos_pacman[1]))[0]
+        #         # print(tmp_dist)
+
+        #     elif actions == Directions.WEST:
+        #         tmp_dist = manhattanDistance(pos_goal, (pos_pacman[0] + 1, pos_pacman[1]))[0]
+
+        #     elif actions == Directions.NORTH:
+        #         tmp_dist = manhattanDistance(pos_goal, (pos_pacman[0], pos_pacman[1] + 1))[0]
+
+        #     if tmp_dist < ref_dist:
+        #         ref_dist = tmp_dist
+        #         best_action = actions
+
+                
+        # return best_action
+            
+
+######################
+    #BROUILLON
+
+          # direction_deltas = {
+        #     Directions.NORTH: (0, 1),
+        #     Directions.SOUTH: (0, -1),
+        #     Directions.EAST: (1, 0),
+        #     Directions.WEST: (-1, 0),
+        #     Directions.STOP: (0, 0)
+        # }
+
+        #best_action = Directions.STOP
+        #min_distance = float('inf')
+        #max_belief_positions = []
+
+        # for i, belief in enumerate(beliefs):
+        #     if eaten[i]:
+        #         continue
+            
+        #     O_t = self.observation_matrix(walls, evidences[i], position)
+            
+        #     belief_updated = self.update(walls, belief, evidences[i], position)
+
+        #     max_belief_position = np.unravel_index(np.argmax(belief_updated), belief_updated.shape)
+        #     max_belief_positions.append(max_belief_position)
+
+        # for action in legal_actions:
+        #     dx, dy = direction_deltas[action]
+        #     new_position = (position[0] + dx, position[1] + dy)
+
+        #     total = 0  
+        #     for max_belief_position in max_belief_positions:
+        #         total += manhattanDistance(new_position, max_belief_position)  
+        #     # distance = manhattanDistance(new_position, max_belief_position)
+
+        #     if total < min_distance:
+        #         min_distance = total
+        #         best_action = action
+
+        # # Retourne la meilleure action
+        # return best_action
+    
+    ######################################################""
 
     def get_action(self, state):
         """Given a Pacman game state, returns a legal move.
